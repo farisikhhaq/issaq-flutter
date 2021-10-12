e185d0bee57dd0e5f1a16ac423e03481693d43b7
SpextrumSecurity <farisikhlasulhaq@yahoo.co.id>
2021-10-12 07:54:08 +0700

    File Update (File Sebelumnya Error mapErrorDebug)

---
+++ android/app/src/main/kotlin/com/example/issaq_pro/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.issaq_pro
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:issaq_pro/screens/auth/Login.dart';
+import 'package:issaq_pro/screens/home/Home.dart';
+import 'package:issaq_pro/widget/widgets.dart';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  @override
+  void initState() {
+    _checkUserSementara(false);
+    super.initState();
+     
+  }
+  Widget build(BuildContext context) {
+   return Scaffold(
+      body: Center(
+        child: wAppLoading(context)
+      ,),
+    );
+  }
+   void _checkUserSementara(bool user) async{
+
+     await Future.delayed(Duration(seconds:2));
+    
+     //navigator
+     Navigator.pushReplacement(context, MaterialPageRoute(builder : (context) => user ? Home() : Login()));
+   
+ } 
+
+}
+
