1a2fed6b718a230b302934019cda44a2969935cd
SpextrumSecurity <farisikhlasulhaq@yahoo.co.id>
2021-10-12 13:15:20 +0700

    Create Quran and News Sidebar

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:issaq_pro/model/navigasi-item.dart';
+import 'package:issaq_pro/page/berita-page.dart';
+import 'package:issaq_pro/page/notifikasi_page.dart';
+import 'package:issaq_pro/page/quran-page.dart';
+import 'package:issaq_pro/page/header_page.dart';
+import 'package:issaq_pro/page/tambahan_page.dart';
+import 'package:issaq_pro/page/doa_page.dart';
+import 'package:issaq_pro/page/sholat_page.dart';
+import 'package:issaq_pro/provider/navigation_provider.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Navigation Drawer';
+
+  @override
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+        create: (context) => NavigationProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: title,
+          theme: ThemeData(primarySwatch: Colors.deepOrange),
+          home: MainPage(),
+        ),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => buildPages();
+
+  Widget buildPages() {
+    final provider = Provider.of<NavigationProvider>(context);
+    final navigationItem = provider.navigationItem;
+
+    switch (navigationItem) {
+      case NavigationItem.header:
+        return HeaderPage();
+      case NavigationItem.quran:
+        return PeoplePage();
+      case NavigationItem.berita:
+        return FavouritesPage();
+      case NavigationItem.sholat:
+        return WorkflowPage();
+      case NavigationItem.doa:
+        return UpdatesPage();
+      case NavigationItem.tambahan:
+        return PluginsPage();
+      case NavigationItem.notifikasi:
+        return NotificationsPage();
+    }
+  }
+}
