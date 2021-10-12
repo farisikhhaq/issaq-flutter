4532ca1fc6193f2da55ac7962f4d804268667d82
SpextrumSecurity <farisikhlasulhaq@yahoo.co.id>
2021-10-12 18:43:55 +0700

    List Artikel Create

---
+++ lib/berita/list_artikel.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:issaq_pro/utils/custom_color.dart';
+
+import 'detail_artikel.dart';
+
+class ListArtikel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Container(
+            height: 603,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: dataArtikelIslam.map((place) {
+                return RaisedButton(
+                  elevation: 0,
+                  color: newColor2,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        PageRouteBuilder(
+                            transitionDuration: Duration(seconds: 1),
+                            transitionsBuilder:
+                                (context, animation, animationTime, child) {
+                              animation = CurvedAnimation(
+                                  parent: animation, curve: Curves.elasticOut);
+                              return ScaleTransition(
+                                  scale: animation,
+                                  alignment: Alignment.center,
+                                  child: child);
+                            },
+                            pageBuilder: (context, animation, animationTime) {
+                              return DetailArtikel(artikelIslami: place);
+                            }));
+                  },
+                  child: Card(
+                    color: Colors.pink[100],
+                    elevation: 2.0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 80.0,
+                            height: 80.0,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: NetworkImage(place.image),
+                                fit: BoxFit.fill,
+                              ),
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  place.title,
+                                  style: styleTitle,
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.schedule, size: 20,),
+                                    Text(
+                                      place.date,
+                                      style: TextStyle(
+                                          fontFamily: 'komik', fontSize: 12),
+                                    ),
+                                    SizedBox(width: 10),
+                                    Icon(Icons.perm_identity,size: 20),
+                                    Expanded(
+                                      child: Text(
+                                        place.author,
+                                        style: TextStyle(
+                                            fontFamily: 'komik', fontSize: 12),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  place.description,
+                                  maxLines: 2,
+                                  style: TextStyle(fontFamily: 'komik'),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/custom_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+Color toscaColor = Color(0xff227f9c);
+Color lightToscaColor = Color(0xff5daecd);
+Color darkToscaColor = Color(0xff00536e);
+Color newColor = Color(0xff6ddccf);
+Color newColor2 = Color(0xffBD82A2);
+Color newColor3 = Color(0xff79708F);
+
+var styleTitle = TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, fontFamily: 'komik');
+var cardTitle = TextStyle(fontWeight: FontWeight.bold, fontFamily: 'komik');
+var tagLine = TextStyle(fontWeight: FontWeight.bold, fontSize: 10, fontFamily: 'komik');
+var listTitle = TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.grey[600], fontFamily: 'komik');
