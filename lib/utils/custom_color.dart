78d36e6e6520582df45d749552d6248643985fc8
SpextrumSecurity <farisikhlasulhaq@yahoo.co.id>
2021-10-17 18:17:47 +0700

    Adding Custom Color

--- lib/utils/custom_color.dart
+++ lib/utils/custom_color.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color toscaColor = Color(0xff227f9c);
 Color lightToscaColor = Color(0xff5daecd);
@@ -11,3 +12,46 @@ var styleTitle = TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0, fontFami
 var cardTitle = TextStyle(fontWeight: FontWeight.bold, fontFamily: 'komik');
 var tagLine = TextStyle(fontWeight: FontWeight.bold, fontSize: 10, fontFamily: 'komik');
 var listTitle = TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.grey[600], fontFamily: 'komik');
+
+
+Color purpleColor = Color(0xff54275E);
+Color orangeColor = Color(0xffFEE1D3);
+
+double edge = 24;
+
+TextStyle blackNormal = GoogleFonts.publicSans(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle whiteNormal = GoogleFonts.publicSans(
+  fontWeight: FontWeight.w500,
+  color: Colors.white
+);
+
+TextStyle blackSemiBold = GoogleFonts.publicSans(
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+TextStyle blacksPoppins = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: Colors.black,
+);
+
+TextStyle orangePoppins = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: orangeColor,
+);
+
+TextStyle purplePoppins = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);
+
+TextStyle whitePoppins = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: Colors.white,
+);
+
+
