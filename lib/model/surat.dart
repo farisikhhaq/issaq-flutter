33380575d4511daaabe4d2ea30e3fee19381577e
xalll <farisikhlasulhaq@yahoo.co.id>
2021-11-07 22:25:44 +0700

    Adding Model for API

---
+++ lib/model/surat.dart
@@ -0,0 +1,23 @@
+class Surat{
+
+  int? nomor;
+  String? nama;
+  String? nama_latin;
+  String? arti;
+  int? jumlah_ayat;
+  String? tempat_turun;
+
+  Surah({this.nomor, this.nama, this.nama_latin, this.arti,
+      this.jumlah_ayat, this.tempat_turun});
+
+  factory  Surah.fromJson(Map<String,dynamic> json){
+    return Surah(
+      nomor: json['nomor'],
+      nama: json['nama'],
+      nama_latin: json['nama_latin'],
+      arti: json['arti'],
+      jumlah_ayat: json['jumlah_ayat'],
+      tempat_turun: json['tempat_turun'],
+    );
+  }
+}
