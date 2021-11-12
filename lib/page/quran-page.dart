0eea7a14ad06d6098f3d372fa4a436c7bf01d052
xalll <farisikhlasulhaq@yahoo.co.id>
2021-11-12 21:52:15 +0700

    Rectify Quran Page

--- lib/page/quran-page.dart
+++ lib/page/quran-page.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:issaq_pro/widget/navigation_drawer_widget.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:issaq_pro/model/translation.dart';
-import 'package:issaq_pro/services/api_services.dart';
-import 'package:issaq_pro/widgets/custom_translation.dart';
-import 'package:issaq_pro/const/constants.dart';
-import 'package:issaq_pro/widgets/custom_translation.dart';
-import 'package:issaq_pro/quran/surah_detail.dart';
-
+import 'package:issaq_pro/quran/surat_list.dart';
+import 'package:issaq_pro/quran/surat_list_detail.dart';
 
 class QuranPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
         drawer: NavigationDrawerWidget(),
         appBar: AppBar(
-          title: Text(
-            'Quran',
-            style: TextStyle(fontFamily: 'sans-serif', color: Colors.black),
-          ),
+          title: Text('Quran'),
           centerTitle: true,
-          backgroundColor: Colors.red[200],
-        ),
-        body: Container(child: Surahdetail()),
+          backgroundColor: Colors.green,
+        ),body:Container(child:SuratList())
+      );
 }
 
 
