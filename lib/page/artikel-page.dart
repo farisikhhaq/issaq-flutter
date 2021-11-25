0f989b1e980e31232ce0c352730579276a0c82e3
xalll <farisikhlasulhaq@yahoo.co.id>
2021-11-25 17:01:23 +0700

    update artikel page

--- lib/page/artikel-page.dart
+++ lib/page/artikel-page.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:issaq_pro/berita/list_artikel.dart';
-import 'package:issaq_pro/widget/navigation_drawer_widget.dart';
 
 class ArtikelPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
-        drawer: NavigationDrawerWidget(),
+        // drawer: NavigationDrawerWidget(),
         appBar: AppBar(
           title: Text(
             'Artikel',
