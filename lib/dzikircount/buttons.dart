bb742d24d9229e425436ce87f3d754892250b711
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-01 22:53:57 +0700

    add buttons + and -

---
+++ lib/dzikircount/buttons.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class Buttons extends StatelessWidget {
+  final Function() tambahButir;
+  final int gantiDzikir;
+  final Function() kurangiButir;
+
+  Buttons(this.tambahButir, this.gantiDzikir, this.kurangiButir);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          MaterialButton(
+            color: Colors.blue,
+            shape: CircleBorder(),
+            onPressed: tambahButir,
+            child: Padding(
+              padding: const EdgeInsets.all(50),
+              child: Text(
+                '+',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+          MaterialButton(
+            color: Colors.blueGrey,
+            shape: CircleBorder(),
+            onPressed: gantiDzikir == 0 ? () {} : kurangiButir,
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                '-',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
