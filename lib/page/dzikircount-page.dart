334c52d03312eb829c81efd035c3bcfd4e3030f5
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-01 22:50:44 +0700

    create dzikir count page

---
+++ lib/page/dzikircount-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:issaq_pro/dzikircount/dzikir_count.dart';
+// import 'package:issaq_pro/widget/navigation_drawer_widget.dart';
+
+class DzikirCountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        // drawer: NavigationDrawerWidget(),
+       /*  appBar: AppBar(
+          title: Text(
+            'Doa',
+            style: TextStyle(fontFamily: 'sans-serif', color: Colors.black),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red[200],
+        ), */
+      );
+}
