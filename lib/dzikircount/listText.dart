ed277ce366219ced28356024744980d85b1960bf
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-01 22:52:32 +0700

    List Text Widget add

---
+++ lib/dzikircount/listText.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ListText extends StatefulWidget {
+  final List text_list;
+  final int dzikirKe;
+
+  ListText(this.text_list, this.dzikirKe);
+
+  @override
+  State<ListText> createState() => _ListTextState();
+}
+
+class _ListTextState extends State<ListText> {
+  String get text => widget.text_list[widget.dzikirKe];
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
