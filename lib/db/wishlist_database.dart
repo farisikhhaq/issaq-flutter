b56ef20975ab2479837dc0c286810e5b69ece997
userthalib <thalibirfan29@gmail.com>
2021-12-03 00:24:24 +0700

    setup konstruktor database

---
+++ lib/db/wishlist_database.dart
@@ -0,0 +1,8 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:issaq_pro/model/wishlist.dart';
+
+class WishlistDatabase {
+  static final WishlistDatabase instance = WishlistDatabase._init();
+  WishlistDatabase._init();
+}
