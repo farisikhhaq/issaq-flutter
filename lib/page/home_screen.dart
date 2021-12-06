998645fcc8910f611238228c7b7ddbdae033a280
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-06 20:52:30 +0700

    home screen add sholat function

--- lib/page/home_screen.dart
+++ lib/page/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:issaq_pro/dzikir/dzikir_page.dart';
 import 'package:issaq_pro/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -135,7 +136,7 @@ class HomeScreen extends StatelessWidget {
                                   Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                          builder: (context) => DoaPage()));
+                                          builder: (context) => DzikirPagiPetang()));
                                 },
                                 child: ItemMenu(
                                   title: 'Dzikir Pagi Petang',
