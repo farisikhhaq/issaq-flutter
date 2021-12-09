2efaee875ccfbcb5d2e89cb8e7abf8ba2e0201de
mashhisyam <haryoh48@gmail.com>
2021-12-09 12:22:46 +0700

    Mengubah warna pada list surah

--- lib/murrotal/audio_surah_screen.dart
+++ lib/murrotal/audio_surah_screen.dart
@@ -3,24 +3,21 @@ import 'package:issaq_pro/model/qari.dart';
 import 'package:issaq_pro/model/surah.dart';
 import 'package:issaq_pro/services/api_services.dart';
 import 'package:issaq_pro/murrotal/audio_screen.dart';
-import 'package:issaq_pro/theme.dart';
+import 'package:issaq_pro/utils/custom_color.dart';
 // import 'package:flutter_quran_app/model/qari.dart';
 // import 'package:flutter_quran_app/model/surah.dart';
 // import 'package:flutter_quran_app/services/api_services.dart';
 // import 'package:flutter_quran_app/view/audio_screen.dart';
 
-
 class AudioSurahScreen extends StatefulWidget {
-  const AudioSurahScreen({Key? key,required this.qari}) : super(key: key);
+  const AudioSurahScreen({Key? key, required this.qari}) : super(key: key);
   final Qari qari;
-
 
   @override
   _AudioSurahScreenState createState() => _AudioSurahScreenState();
 }
 
 class _AudioSurahScreenState extends State<AudioSurahScreen> {
-
   ApiServices apiServices = ApiServices();
 
   @override
@@ -29,29 +26,30 @@ class _AudioSurahScreenState extends State<AudioSurahScreen> {
       appBar: AppBar(
         backgroundColor: orangeColor,
         elevation: 0,
-        title: Text('Surah List',style: blacksPoppins.copyWith(fontSize: 15)),
+        title: Text('Surah List', style: blacksPoppins.copyWith(fontSize: 20)),
       ),
-      body:  SafeArea(
+      body: SafeArea(
         child: FutureBuilder(
           future: apiServices.getSurah(),
-          builder: (BuildContext context,
-              AsyncSnapshot<List<Surah>> snapshot) {
+          builder: (BuildContext context, AsyncSnapshot<List<Surah>> snapshot) {
             if (snapshot.hasData) {
               List<Surah>? surah = snapshot.data;
               return ListView.builder(
-                itemCount: surah!.length,
-                itemBuilder: (context, index) =>
-                    AudioTile(surahName:snapshot.data![index].englishName,
-                        totalAya: snapshot.data![index].numberOfAyahs,
-                        number: snapshot.data![index].number,
-                        onTap: (){
-                       Navigator.push(context, MaterialPageRoute(builder:
-                           (context)=>AudioScreen(
-                             qari: widget.qari,
-                             index: index+1,
-                             list: surah,
-                           )));
-                        }));
+                  itemCount: surah!.length,
+                  itemBuilder: (context, index) => AudioTile(
+                      surahName: snapshot.data![index].englishName,
+                      totalAya: snapshot.data![index].numberOfAyahs,
+                      number: snapshot.data![index].number,
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AudioScreen(
+                                      qari: widget.qari,
+                                      index: index + 1,
+                                      list: surah,
+                                    )));
+                      }));
             }
             return Center(
               child: CircularProgressIndicator(),
@@ -63,7 +61,11 @@ class _AudioSurahScreenState extends State<AudioSurahScreen> {
   }
 }
 
-Widget AudioTile({required String? surahName,required totalAya,required number ,required VoidCallback onTap}){
+Widget AudioTile(
+    {required String? surahName,
+    required totalAya,
+    required number,
+    required VoidCallback onTap}) {
   return GestureDetector(
     onTap: onTap,
     child: Padding(
@@ -71,49 +73,55 @@ Widget AudioTile({required String? surahName,required totalAya,required number ,
       child: Container(
         padding: EdgeInsets.all(16),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 5,
-              spreadRadius: 0,
-              color: Colors.black12,
-              offset: Offset(0,3),
-            ),
-          ]
-        ),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+            color: purpleColor,
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 5,
+                spreadRadius: 0,
+                color: Colors.black12,
+                offset: Offset(0, 3),
+              ),
+            ]),
         child: Row(
           children: [
             Container(
               alignment: Alignment.center,
-              height: 30,
-              width: 40,
+              height: 40,
+              width: 30,
               padding: EdgeInsets.all(8),
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: Colors.black,
+                color: Colors.white,
               ),
               child: Text((number).toString(),
-                style: TextStyle(color:Colors.white ,fontSize: 16,fontWeight: FontWeight.bold),),
+                  style: blacksPoppins.copyWith(
+                      fontSize: 13, fontWeight: FontWeight.bold)),
             ),
-            SizedBox(width: 20,),
+            SizedBox(
+              width: 20,
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  surahName!,
-                  textAlign: TextAlign.end,
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
+                Text(surahName!,
+                    textAlign: TextAlign.end,
+                    style: blacksPoppins.copyWith(
+                        fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white)),
+                SizedBox(
+                  height: 3,
                 ),
-                SizedBox(height: 3,),
-                Text("Total Aya : $totalAya" ,style: TextStyle(color: Colors.black54,fontSize: 16),),
+                Text(
+                  "Total Aya : $totalAya",
+                  style: blacksPoppins.copyWith(color: Colors.white, fontSize: 15),
+                ),
               ],
             ),
             Spacer(),
-            Icon(Icons.play_circle_fill,color: Color(0xffA5D2D6),),
+            Icon(
+              Icons.play_circle_fill,
+              color: Colors.white,
+            ),
           ],
         ),
       ),
