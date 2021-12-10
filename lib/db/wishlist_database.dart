ca880e78b141ccb0e76537f55387fef4d30784c2
userthalib <thalibirfan29@gmail.com>
2021-12-10 12:54:07 +0700

    merapikan kode pada wishlist database

--- lib/db/wishlist_database.dart
+++ lib/db/wishlist_database.dart
@@ -1,27 +1,29 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:issaq_pro/model/wishlist.dart';
+import 'package:issaq_pro/model/wishlist_model.dart';
 
 class WishlistDatabase {
   static final WishlistDatabase instance = WishlistDatabase._init();
 
-  static Database?_database;
+  static Database? _database;
 
   WishlistDatabase._init();
 
   Future<Database> get database async {
-    if (_database !=null) return _database!;
+    if (_database != null) return _database!;
 
     _database = await _initDB('wishlist.db');
     return _database!;
   }
-  Future<Database> _initDB(String filePath)  async {
+
+  Future<Database> _initDB(String filePath) async {
     final dbPath = await getDatabasesPath();
     final path = join(dbPath, filePath);
     return await openDatabase(path, version: 1, onCreate: _createDB);
   }
+
   Future _createDB(Database db, int version) async {
-  final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    final idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
     final textType = 'TEXT NOT NULL';
     final boolType = 'BOOLEAN NOT NULL';
     final integerType = 'INTEGER NOT NULL';
@@ -38,9 +40,8 @@ CREATE TABLE $tableWishlist (
 ''');
   }
 
-Future<Wishlist> create(Wishlist wishlist) async {
+  Future<Wishlist> create(Wishlist wishlist) async {
     final db = await instance.database;
-
 
     final id = await db.insert(tableWishlist, wishlist.toJson());
     return wishlist.copy(id: id);
