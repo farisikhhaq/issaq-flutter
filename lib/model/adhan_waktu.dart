1aab9f09690d8e0ae7c7ffc4c0a0567e0141940b
xalll <farisikhlasulhaq@yahoo.co.id>
2021-12-14 22:42:44 +0700

    add model adzan and add dependency

---
+++ lib/model/adhan_waktu.dart
@@ -0,0 +1,13 @@
+import 'package:adhan_dart/adhan_dart.dart';
+
+adzanwaktu() {
+  Coordinates coordinates = Coordinates(-7.983908, 112.621391);
+  DateTime date = DateTime.now();
+  CalculationParameters params = CalculationMethod.Dubai();
+  PrayerTimes prayerTimes =
+      PrayerTimes(coordinates, date, params, precision: true);
+  DateTime? fajrTime = prayerTimes.fajr!.toLocal();
+  print(fajrTime);
+  double degree = Qibla.qibla(coordinates);
+  print(degree);
+}
