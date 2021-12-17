8350872d6069f9e79e16e9e6504237794476d0ff
userthalib <thalibirfan29@gmail.com>
2021-12-17 11:41:48 +0700

    membuat class wishlist page

---
+++ lib/page/wishlist_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:issaq_pro/db/wishlist_database.dart';
+import 'package:issaq_pro/model/wishlist_model.dart';
+import 'package:issaq_pro/wishlist/edit_wishlist_page.dart';
+import 'package:issaq_pro/wishlist/wishlist_page_detail.dart';
+import 'package:issaq_pro/widget/wishlist_card_widget.dart';
+
+class WishlistsPage extends StatefulWidget {
+  @override
+  _WishlistsPageState createState() => _WishlistsPageState();
+}
+
+class _WishlistsPageState extends State<WishlistsPage> {
+  late List<Wishlist> wishlists;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshWishlists();
+  }
+
+  @override
+  void dispose() {
+    WishlistDatabase.instance.close();
+
+    super.dispose();
+  }
+
+  Future refreshWishlists() async {
+    setState(() => isLoading = true);
+
+    this.wishlists = await WishlistDatabase.instance.readAllWishlists();
+
+    setState(() => isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Wishlists',
+            style: TextStyle(fontSize: 24),
+          ),
+          actions: [Icon(Icons.search), SizedBox(width: 12)],
+        ),
+        body: Center(
+          child: isLoading
+              ? CircularProgressIndicator()
+              : wishlists.isEmpty
+                  ? Text(
+                      'No Wishlists',
+                      style: TextStyle(color: Colors.white, fontSize: 24),
+                    )
+                  : buildWishlists(),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          child: Icon(Icons.add),
+          onPressed: () async {
+            await Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => AddEditWishlistPage()),
+            );
+
+            refreshWishlists();
+          },
+        ),
+      );
+
+  
+}
